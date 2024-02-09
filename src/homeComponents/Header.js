1134d76bcf869dd38874879679eca486a2dc697a
thameemansari <thameemrpp@gmail.com>
2024-02-09 13:19:45 +0530

    todayyaa final

--- src/homeComponents/Header.js
+++ src/homeComponents/Header.js
@@ -6,6 +6,8 @@ import { FaPlay } from "react-icons/fa";
 import { FaArrowRight } from "react-icons/fa";
 import { NavLink } from 'react-router-dom';
 import Timeline from './Timeline.js'
+import { GoChevronRight } from "react-icons/go";
+import { Link } from 'react-scroll';
 const Header = () => {
   return (
     <div className='time-line-header'>
@@ -22,13 +24,49 @@ const Header = () => {
            <NavLink to='/Hiring'><button className='btn'>More About Us</button></NavLink> 
             <FaPlay className='play-btn'/>
         </div>
+        <ul className='navbar-ul-hdr'>
+                    <li>Get your stories featured on Story Club in just Rs 49.</li>
+                    
+                </ul>
         <h1  data-aos="fade-down" data-aos-easing="linear"
      data-aos-duration="1000">Real People . Real Stories</h1>
             <p data-aos="fade-up" data-aos-easing="linear"
       >Everybody has a Story to tell</p>
         </div> 
-       
+      <Link to='connect'> <button className="button">
+        
+  <div className="line-one">
+    <h1>swipe Here..</h1>
+  <GoChevronRight />
+  <GoChevronRight />
+  <GoChevronRight />
+  <GoChevronRight />
 
+  </div>
+  {/* <div class="line two">
+  <GoChevronRight />
+
+  </div>
+  <div class="line three">
+  <GoChevronRight />
+  </div>
+    <div class="line four">
+    <GoChevronRight />
+  </div>
+    
+    <div class="line five">
+    <GoChevronRight />
+  </div>
+    
+    <div class="line six">
+    <GoChevronRight />
+  </div>
+    
+    <div class="line seven">
+    <GoChevronRight />
+  </div> */}
+</button>
+</Link> 
       <div className='header-btns-for-redirect'>
       <div className="btn-1">
           <NavLink to='/'><button>Audio Stories</button></NavLink>
@@ -40,7 +78,9 @@ const Header = () => {
           <FaArrowRight />
         
           </div>
+          
       </div>
+      
         <div className="two-header-btns">
          
         </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Header from '../homeComponents/Header'
 import Navbar from '../homeComponents/Navbar'
 import Connectingpage from '../homeComponents/Connectingpage.js'
 import Workshop from '../homeComponents/Workshop.js'
 import '../css/Home.css'
+import LocomotiveScroll from 'locomotive-scroll'
+
 const Home = () => {
+  
   return (
     <div>
        <div className="horizontal-scroll-container">
       <div className="horizontal-scroll-content">
-        <Header />
+        <Header  />
         <Connectingpage />
         {/* <Workshop /> */}
       </div>
